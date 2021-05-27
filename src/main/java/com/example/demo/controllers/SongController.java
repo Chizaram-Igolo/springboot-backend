1899e2855e374dc52e0fd13c373d54eaa2be0d9e
Chizaram-Igolo <chizaram.igolo@yahoo.com>
2021-05-27 12:12:12 +0100

    add cors

--- src/main/java/com/example/demo/controllers/SongController.java
+++ src/main/java/com/example/demo/controllers/SongController.java
@@ -2,13 +2,14 @@ package com.example.demo.controllers;
 import com.example.demo.model.Song;
 import com.example.demo.services.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
+@CrossOrigin( origins = "*" )
 @RestController
 @RequestMapping("/api")
 public class SongController {
