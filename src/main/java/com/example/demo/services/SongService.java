18532ac9d24fdc34592572660a456c18b826dad1
Chizaram-Igolo <chizaram.igolo@yahoo.com>
2021-05-27 12:02:49 +0100

    remove cors

--- src/main/java/com/example/demo/services/SongService.java
+++ src/main/java/com/example/demo/services/SongService.java
@@ -3,10 +3,8 @@ package com.example.demo.services;
 import com.example.demo.model.Song;
 import com.example.demo.repositories.SongRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
 import java.util.List;
-@CrossOrigin( origins = "*" )
+
 @Service
 public class SongService {
 
